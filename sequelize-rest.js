bb1a4a13fcd6f2c643ffbe8ef41eea37c95f6ae6
mariuspatrick <marius.patrick72@gmail.com>
2019-12-07 18:34:07 +0100

    Added all 5 RESTful actions

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -3,45 +3,30 @@ const Movie = require("./model");
 
 const router = new Router();
 
-//I tried inserting the movies while creating the table
-
-const movies = [
-  {
-    id: 1,
-    title: "Star Wars",
-    yearOfRelease: "1977",
-    synopsis:
-      "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader"
-  },
-  {
-    id: 2,
-    title: "Harry Potter and the Sorcerer's Stone",
-    yearOfRelease: "2001",
-    synopsis:
-      "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world."
-  },
-  {
-    id: 3,
-    title: "The Matrix",
-    yearOfRelease: "1999",
-    synopsis:
-      "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers"
-  },
-  {
-    id: 4,
-    title: "Toy Story",
-    yearOfRelease: "1995",
-    synopsis:
-      "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room."
-  }
-];
-
 router.post("/movie", (req, res, next) => {
-  console.log("Movies here: ", movies);
-  console.log("req.body here: ", req.body);
-  Movie.create(movies.forEach(movie => movie))
+  Movie.create(req.body)
     .then(res)
     .catch(err => next(err));
+});
+
+router.get("/movie", (req, res) => {
+  Movie.findAll().then(result => res.send(result));
+});
+
+router.get("/movie/:id", (req, res) => {
+  Movie.findByPk(req.params.id).then(movie => res.send(movie));
+});
+
+router.put("/movie/:id", (req, res, next) => {
+  Movie.findByPk(req.params.id)
+    .then(movie => movie.update(req.body))
+    .then(updatedMovie => res.send(updatedMovie));
+});
+
+router.delete("/movie/:id", (req, res, next) => {
+  Movie.destroy({ where: { id: req.params.id } })
+    .then(movie => res.send({ movie }))
+    .catch(next);
 });
 
 module.exports = router;
