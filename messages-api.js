fc70904fa292f3c88b6b4696ac09273d2a2b9a5d
mariuspatrick <marius.patrick72@gmail.com>
2019-12-07 14:19:10 +0100

    Adding error handlers

--- messages-api.js
+++ messages-api.js
@@ -8,11 +8,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.get("/message", (req, res) => res.send("Hello"));
 
-app.post("/messages", (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: "Message received loud and clear"
-  });
+app.post("/messages", (req, res, next) => {
+  if (req.body === {}) {
+    res.status(400).end();
+  } else {
+    console.log(req.body);
+    res.json({
+      message: "Message received loud and clear"
+    });
+  }
 });
 
 app.listen(port, console.log(`Listening on port: ${port}`));
